c3b9b5400844e0fdc3c251435cc6ee704b4846f4
jpbrito <jpbrito@latam.stefanini.com>
2024-02-24 16:49:08 -0300

    teste tutorial

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api'
+/*
 import express from 'express'
 import Redis from './src/services/redis.js'
 import ngrok from "@ngrok/ngrok";
@@ -144,3 +145,42 @@ bot.on('message', async (msg) => {
     console.log('[end] ->  ACABOU PORRA!');
   }
 });
+*/
+
+export default async (request, response) => {
+  try {
+      // Create our new bot handler with the token
+      // that the Botfather gave us
+      // Use an environment variable so we don't expose it in our code
+      const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+
+      // Retrieve the POST request body that gets sent from Telegram
+      const { body } = request;
+
+      // Ensure that this is a message being sent
+      if (body.message) {
+          // Retrieve the ID for this chat
+          // and the text that the user sent
+          const { chat: { id }, text } = body.message;
+
+          // Create a message to send back
+          // We can use Markdown inside this
+          const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+
+          // Send our new message back in Markdown and
+          // wait for the request to finish
+          await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+      }
+  }
+  catch(error) {
+      // If there was an error sending our message then we 
+      // can log it into the Vercel console
+      console.error('Error sending message');
+      console.log(error.toString());
+  }
+  
+  // Acknowledge the message with Telegram
+  // by sending a 200 HTTP status code
+  // The message here doesn't matter.
+  response.send('OK');
+};
